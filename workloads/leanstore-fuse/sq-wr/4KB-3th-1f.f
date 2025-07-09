80ad8f7c7a163e0dac36a29b92e45cc60f34050e
Khoa Nguyen <khoa.nguyen@tum.de>
2025-07-09 19:16:05 +0200

    WIP

---
+++ workloads/leanstore-fuse/sq-wr/4KB-3th-1f.f
@@ -0,0 +1,22 @@
+set mode quit alldone
+set $dir=/home/khoa/mnt/test4
+set $nthreads=3
+
+define file name=bigfile, path=$dir
+
+define process name=fileopen, instances=1
+{
+        thread name=fileopener, memsize=4k, instances=$nthreads
+        {
+                flowop createfile name=create1, filesetname=bigfile
+                flowop write name=write-file, filesetname=bigfile, iosize=4k, iters=25600
+                flowop closefile name=close1
+                flowop finishoncount name=finish, value=1
+        }
+}
+create files
+system "sync"
+system "echo 3 > /proc/sys/vm/drop_caches"
+system "echo started >> cpustats.txt"
+system "echo started >> diskstats.txt"
+run
---
+++ workloads/leanstore-fuse/sq-wr/4KB-3th-3f.f
@@ -0,0 +1,42 @@
+set mode quit alldone
+set $dir=/home/khoa/mnt/test4
+set $nfiles=3
+set $meandirwidth=3
+set $nthreads=1
+set $io_size=4k
+set $iterations=25600
+
+define fileset name=bigfileset, path=$dir, entries=$nfiles, dirwidth=$meandirwidth,dirgamma=0,size=2g
+
+define process name=filesequentialwrite, instances=1
+{
+        thread name=filewriter, memsize=$io_size, instances=$nthreads
+        {
+                flowop createfile name=create1, filesetname=bigfileset, fd=1, indexed=1
+                flowop write name=write-file1, filesetname=bigfileset, iosize=$io_size,iters=$iterations, fd=1, indexed=1
+                flowop closefile name=close1, fd=1, indexed=1
+                flowop finishoncount name=finish, value=1
+        }
+
+        thread name=filewriter, memsize=$io_size, instances=$nthreads
+        {
+                flowop createfile name=create2, filesetname=bigfileset, fd=1, indexed=2
+                flowop write name=write-file2, filesetname=bigfileset, iosize=$io_size,iters=$iterations, fd=1, indexed=2
+                flowop closefile name=close2, fd=1, indexed=2
+                flowop finishoncount name=finish, value=1
+        }
+
+        thread name=filewriter, memsize=$io_size, instances=$nthreads
+        {
+                flowop createfile name=create3, filesetname=bigfileset, fd=1, indexed=3
+                flowop write name=write-file3, filesetname=bigfileset, iosize=$io_size,iters=$iterations, fd=1, indexed=3
+                flowop closefile name=close3, fd=1, indexed=3
+                flowop finishoncount name=finish, value=1
+        }
+}
+create files
+system "sync"
+system "echo 3 > /proc/sys/vm/drop_caches"
+system "echo started >> cpustats.txt"
+system "echo started >> diskstats.txt"
+run
