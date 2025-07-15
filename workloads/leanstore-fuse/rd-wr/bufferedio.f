a4d8e5acc78bc1c7159fb726b5841054602a62dc
Khoa Nguyen <khoa.nguyen@tum.de>
2025-07-15 21:18:21 +0200

    rd-wr

--- workloads/leanstore-fuse/rd-wr/bufferedio.f
+++ workloads/leanstore-fuse/rd-wr/bufferedio.f
@@ -1,27 +1,28 @@
 set mode quit alldone
 set $dir=/mnt/leanfs
+set $iosize=4k
 set $nfiles=1
 set $meandirwidth=1
 set $nthreads=1
 #I/O amount equal to 60 G(SSD)
-set $memsize=4k
-set $iterations=262144
+set $iterations=26214400
 
-define file name=bigfileset, path=$dir, size=1g, prealloc
+define file name=bigfileset, path=$dir, size=10m, prealloc
 
 define process name=fileopen, instances=1
 {
-        thread name=fileopener, memsize=$memsize, instances=$nthreads
+        thread name=fileopener, memsize=$iosize, instances=$nthreads
         {
                 flowop openfile name=open1, filesetname=bigfileset, fd=1
-                flowop write name=write-file, filesetname=bigfileset, random, iosize=$memsize, iters=$iterations, fd=1
+                flowop write name=write-file, filesetname=bigfileset, random, iosize=$iosize, iters=$iterations, fd=1
                 flowop closefile name=close1, fd=1
                 flowop finishoncount name=finish, value=1
         }
 }
 create files
+
 system "sync"
 system "echo 3 > /proc/sys/vm/drop_caches"
-system "echo started >> cpustats.txt"
-system "echo started >> diskstats.txt"
-run 60
+
+system "iostat -xy 1 > stat.log &"
+run 10
---
+++ workloads/leanstore-fuse/rd-wr/directio.f
@@ -0,0 +1,29 @@
+set mode quit alldone
+set $dir=/mnt/leanfs
+set $iosize=4k
+set $nfiles=1
+set $meandirwidth=1
+set $nthreads=1
+#I/O amount equal to 60 G(SSD)
+set $iterations=26214400
+
+define file name=bigfileset, path=$dir, size=10m, prealloc
+
+define process name=fileopen, instances=1
+{
+        thread name=fileopener, memsize=2m, instances=$nthreads
+        {
+                flowop openfile name=open1, directio, filesetname=bigfileset, fd=1
+                flowop write name=write-file, directio, filesetname=bigfileset,
+random, iosize=$iosize, iters=$iterations, fd=1
+                flowop closefile name=close1, fd=1
+                flowop finishoncount name=finish, value=1
+        }
+}
+create files
+
+system "sync"
+system "echo 3 > /proc/sys/vm/drop_caches"
+
+system "iostat -xy 1 > stat.log &"
+run 10
